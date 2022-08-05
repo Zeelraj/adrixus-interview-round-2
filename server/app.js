95591fcc029c529efc6d968e2ffc4d76cdbf52a5
Zeelrajsinh Mahida <zeelrajsinhmahida@Zeelrajsinhs-MacBook-Pro.local>
2022-08-06 01:22:01 +0530

    App-Server: Enable CORS

--- server/app.js
+++ server/app.js
@@ -24,6 +24,32 @@ app.use(express.urlencoded({ extended: true }));
 // cookies and file middleware
 app.use(cookieParser());
 
+// Allows Different Servers to access routes - Enable CORS
+app.use(function (req, res, next) {
+  header = req.headers.origin || "http://localhost:3000";
+  console.log(`Header: ${header}`);
+  res.setHeader("Access-Control-Allow-Origin", header);
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type,x-access-token"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 // morgan middleware
 app.use(morgan("tiny"));
 
